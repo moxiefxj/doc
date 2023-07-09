0b9ef0d148bf9bc26802460bccd6e8a4611914b9
[zf19980903] <[1911085221@qq.com]>
2023-07-09 14:46:07 +0800

    🎉 init: 初始化

---
+++ .vuepress/config.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: fengxiaojing
+ * @Date: 2023-04-05 22:44:24
+ * @LastEditors: fengxiaojing
+ * @LastEditTime: 2023-04-05 22:46:03
+ * @Description: file content
+ */
+module.exports = {
+    title: 'Hello VuePress',
+    description: 'Just playing around'
+}
