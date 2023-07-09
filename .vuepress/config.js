ce8bcb672a0168f430b54f2504af60dcbffbbad0
[zf19980903] <[1911085221@qq.com]>
2023-07-09 14:56:35 +0800

    🐎 ci: 测试页面

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -2,10 +2,11 @@
  * @Author: fengxiaojing
  * @Date: 2023-04-05 22:44:24
  * @LastEditors: fengxiaojing
- * @LastEditTime: 2023-04-05 22:46:03
+ * @LastEditTime: 2023-07-09 14:47:55
  * @Description: file content
  */
 module.exports = {
     title: 'Hello VuePress',
-    description: 'Just playing around'
+    description: 'Just playing around',
+    base: '/doc/',
 }
